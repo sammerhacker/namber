830d047ba498d986000a3f50203e71773a66b011
sayser <68947652+sammerhacker@users.noreply.github.com>
2022-03-23 21:53:28 +0700

    Create number.py

---
+++ number.py
@@ -0,0 +1,57 @@
+tr=int(input("สูตรคูณเเม่อะไร : "))
+
+print (" ")
+
+x=1*+tr
+
+t=2*+tr
+
+f=3*+tr
+
+z=4*+tr
+
+s=5*+tr
+
+a=6*+tr
+
+h=7*+tr
+
+v=8*+tr
+
+b=9*+tr
+
+j=10*+tr
+
+l=11*+tr
+
+i=12*+tr
+
+print ("========================]")
+
+print ("        เเม่ = "+str(tr))
+
+print (" "+str(tr)+" × 1 = "+str(x))
+
+print (" "+str(tr)+" × 2 = "+str(t))
+
+print (" "+str(tr)+" × 3 = "+str(f))
+
+print (" "+str(tr)+" × 4 = "+str(z))
+
+print (" "+str(tr)+" × 5 = "+str(s))
+
+print (" "+str(tr)+" × 6 = "+str(a))
+
+print (" "+str(tr)+" × 7 = "+str(h))
+
+print (" "+str(tr)+" × 8 = "+str(v))
+
+print (" "+str(tr)+" × 9 = "+str(b))
+
+print (" "+str(tr)+" × 10 = "+str(j))
+
+print (" "+str(tr)+" × 11 = "+str(l))
+
+print (" "+str(tr)+" × 12 = "+str(i))
+
+print ("========================]")
